cc9c2dc53d32955e455550629e8c7ddfa477fa64
Iminha <justinschrier@gmail.com>
2023-01-11 13:01:49 -0800

    flamespire sq, debug lines

--- src/main/java/iminhak/ewex/EX5.java
+++ src/main/java/iminhak/ewex/EX5.java
@@ -9,17 +9,19 @@ import gg.xp.reevent.scan.HandleEvents;
 import gg.xp.xivdata.data.duties.KnownDuty;
 import gg.xp.xivsupport.callouts.CalloutRepo;
 import gg.xp.xivsupport.callouts.ModifiableCallout;
-import gg.xp.xivsupport.events.actlines.events.AbilityCastStart;
-import gg.xp.xivsupport.events.actlines.events.HeadMarkerEvent;
-import gg.xp.xivsupport.events.actlines.events.TetherEvent;
+import gg.xp.xivsupport.events.actlines.events.*;
+import gg.xp.xivsupport.events.actlines.events.vfx.StatusLoopVfxApplied;
 import gg.xp.xivsupport.events.misc.pulls.PullStartedEvent;
 import gg.xp.xivsupport.events.state.XivState;
 import gg.xp.xivsupport.events.state.combatstate.StatusEffectRepository;
 import gg.xp.xivsupport.events.triggers.seq.SequentialTrigger;
 import gg.xp.xivsupport.events.triggers.seq.SqtTemplates;
+import gg.xp.xivsupport.models.XivPlayerCharacter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.util.List;
 import java.util.Map;
 
 @CalloutRepo(name = "Rubicante Extreme", duty = KnownDuty.RubicanteEx)
@@ -41,6 +43,12 @@ public class EX5 extends AutoChildEventHandler implements FilteredEventHandler {
     private final ModifiableCallout<AbilityCastStart> radialFlagration = ModifiableCallout.durationBasedCall("Radial Flagration", "Proteans");
     private final ModifiableCallout<TetherEvent> ghastlyWind = new ModifiableCallout<>("Ghastly Wind", "Point tether out");
     private final ModifiableCallout<TetherEvent> shatteringHeatTether = new ModifiableCallout<>("Shattering Heat tether", "Tank tether on YOU");
+    private final ModifiableCallout<AbilityCastStart> sweepingImmolationSpread = ModifiableCallout.durationBasedCall("Sweeping Immolation: Spread", "Behind and Spread");
+    private final ModifiableCallout<AbilityCastStart> sweepingImmolationStack = ModifiableCallout.durationBasedCall("Sweeping Immolatiom: Stack", "Behind and Stack");
+
+    private final ModifiableCallout<BuffApplied> flamespireOut = new ModifiableCallout<>("Flamespire Brand: Flare", "Flare, out soon");
+    private final ModifiableCallout<BuffApplied> flamespireIn = new ModifiableCallout<>("Flamespire Brand: Nothing", "Stack middle soon");
+    private final ModifiableCallout<BuffApplied> flamespireSpread = new ModifiableCallout<>("Flamespire Brand: Spread", "Spread");
 
     public EX5(XivState state, StatusEffectRepository buffs) {
         this.state = state;
@@ -100,6 +108,8 @@ public class EX5 extends AutoChildEventHandler implements FilteredEventHandler {
             case 0x7D2E -> call = dualfire;
             case 0x7D2D -> call = shatteringHeat;
             case 0x7CFE -> call = radialFlagration;
+            case 0x7D20 -> call = sweepingImmolationSpread;
+            case 0x7D21 -> call = sweepingImmolationStack;
             default -> {
                 return;
             }
@@ -110,7 +120,6 @@ public class EX5 extends AutoChildEventHandler implements FilteredEventHandler {
     @HandleEvents
     public void headMarker(EventContext context, HeadMarkerEvent event) {
         int offset = getHeadmarkerOffset(event);
-        log.info("HEADMARKER FOUND WITH OFFSET: {}", offset);
         final ModifiableCallout<HeadMarkerEvent> call;
         if(offset >= -263 && offset <= -256 && event.getTarget().isThePlayer()) {
             context.accept(limitCutNumber.getModified(event, Map.of("number", offset + 264)));
@@ -138,5 +147,58 @@ public class EX5 extends AutoChildEventHandler implements FilteredEventHandler {
             ace -> ace.abilityIdMatches(0x7D13),
             (e1, s) -> {
                 log.info("Flamespire Brand: Start");
+                List<BuffApplied> stack = s.waitEventsQuickSuccession(4, BuffApplied.class, ba -> ba.buffIdMatches(0xD9C), Duration.ofMillis(200));
+                if(stack.contains(getState().getPlayer())) {
+                    s.accept(flamespireOut.getModified());
+                } else {
+                    s.accept(flamespireIn.getModified());
+                }
+
+                log.info("Flamespire Brand: Waiting for stack to drop");
+                s.waitEvent(BuffRemoved.class, br -> br.buffIdMatches(0xD9C)); //ID for stack debuff
+                s.accept(flamespireSpread.getModified());
             });
+
+    //Possibilities:
+    //Status loop vfx (noticed 21E, 221, 222)
+    //circles used 8024 when starting purg, and 8025 when ending
+    @AutoFeed
+    public SequentialTrigger<BaseEvent> purgatorySq = SqtTemplates.multiInvocation(13_000, AbilityCastStart.class,
+            ace -> ace.abilityIdMatches(0x80E9));
+
+    //DEBUG
+    @HandleEvents
+    public void debugUsed(EventContext context, AbilityUsedEvent event) {
+        int id = (int) event.getAbility().getId();
+        String source = event.getSource().toString();
+        switch (id) {
+            case 0x8024 -> log.info("IminhaDebug: AUE 0x8024 by {}", source);
+            case 0x7efa -> log.info("IminhaDebug: AUE 0x7efa by {}", source);
+            case 0x7efb -> log.info("IminhaDebug: AUE 0x7efb by {}", source);
+            case 0x7cbf -> log.info("IminhaDebug: AUE 0x7cbf by {}", source);
+            case 0x8025 -> log.info("IminhaDebug: AUE 0x8025 by {}", source);
+            case 0x7cc0 -> log.info("IminhaDebug: AUE 0x7cc0 by {}", source);
+        }
+    }
+
+    @HandleEvents
+    public void debugVFX(EventContext context, StatusLoopVfxApplied event) {
+        int id = (int) event.getStatusLoopVfx().getId();
+        String source = event.getTarget().toString();
+        switch (id) {
+            case 542 -> log.info("IminhaDebug: VFX 542 on {}", source);
+            case 543 -> log.info("IminhaDebug: VFX 543 on {}", source);
+            case 544 -> log.info("IminhaDebug: VFX 544 on {}", source);
+            case 545 -> log.info("IminhaDebug: VFX 545 on {}", source);
+            case 546 -> log.info("IminhaDebug: VFX 546 on {}", source);
+        }
+    }
+
+    @HandleEvents
+    public void appliedDebug(EventContext context, BuffApplied event) {
+        int stacks = (int) event.getRawStacks();
+        String source = event.getSource().toString();
+        if(event.getBuff().getId() == 0x891)
+            log.info("IminhaDebug: 891 buff, {} stacks on {}", stacks, source);
+    }
 }
